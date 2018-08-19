4585e08aed584daf1aa19b4c899c177abec883c7
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-19 21:08:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
+
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
@@ -19,6 +20,7 @@ function moveDodgerRight() {
 document.addEventListener('keydown', function(key) {
   if (key.which === 37) {
     moveDodgerLeft()
+    alert('hi')
   }
   else (key.which === 39) {
     moveDodgerRight()
