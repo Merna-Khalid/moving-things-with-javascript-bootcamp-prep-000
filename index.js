294fc64d0bb7ab457edb64f6013d1d686fbe36bc
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-19 21:15:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
-  if (right >0) {
+  if (right > 0) {
     dodger.style.right = '${right - 1}px'
   }
 }
+
 document.addEventListener('keydown', function(key) {
   alert('key pressed')
   if (key.which === LEFT_ARROW) {
